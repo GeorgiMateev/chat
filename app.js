4456a91a630f9f8450843785a5d6157841e0b60f
Georgi Mateev <gmmateev92@abv.bg>
2014-06-10 20:03:22 +0300

    Added comments in the server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
@@ -7,8 +7,13 @@ var session = require('express-session');
 var errorHandler = require('errorhandler');
 var path = require('path');
 
+//Ensures that all messages for a socket will be sent to the process where the socket is hosted
 var sticky = require('sticky-session');
+
+//Allows messages to be sent to sockets hosted on other processes
 var redis = require('socket.io-redis');
+
+//A new process will be forked for each logical CPU
 var cpus = require('os').cpus().length;
 
 //Parse command line parameters
@@ -19,7 +24,7 @@ if (argv.s) {
     // Run the application in a single thread
     var server = setupApp();
     server.listen(3000, function () {
-        console.log("Server started on a single thread on 3000 port");
+        console.log("Server started on a single process with pid " + process.pid +" on 3000 port");
     });
 }
 else {
@@ -28,10 +33,10 @@ else {
     sticky(function (cpus) {
         // A new proccess will be forked for every cpu
         // This code will be executed only in slave workers
-
+        console.log("A process with pid "+ process.pid +" has been forked.");
         return setupApp();
     }).listen(3000, function () {
-        console.log('server started on multiple processes on 3000 port');
+        console.log('Server started on multiple processes on 3000 port');
     });
 }
 
@@ -41,7 +46,7 @@ function setupApp () {
     // all environments
     app.set('port', process.env.PORT || 3000);
     app.set('views', path.join(__dirname, 'views'));
-    app.use(favicon());
+    //app.use(favicon(__dirname + '/public/favicon.ico'));
     app.use(logger('dev'));
     app.use(methodOverride());
     app.use(cookieParser('your secret here'));
